744cbc7796c0ac6477cd803818713ea3b20b2db6
YJB-Gay <129620824+YJB-Gay@users.noreply.github.com>
2024-01-15 13:30:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ require("dotenv").config();
 const fs = require("fs");
 const axios = require("axios");
 
-const subreddit = "r/discordapp";
+// Define an array of subreddit objects with their corresponding thread IDs
+const subreddits = [
+    { name: "r/SalC1", threadId: "1196236674439589998" },
+    { name: "r/2b2t_Uncensored", threadId: "1196236581850329200" },
+    { name: "r/minecraftclients", threadId: "1196236494529114212" },
+    { name: "r/MinecraftExploits", threadId: "1196236433774629046" },
+    { name: "r/2b2t", threadId: "1196236623470399558" },
+];
+
 let lastPost;
 
-if(fs.existsSync("last.dat")) {
-	lastPost = JSON.parse(fs.readFileSync("last.dat").toString());
+if (fs.existsSync("last.dat")) {
+    lastPost = JSON.parse(fs.readFileSync("last.dat").toString());
 }
 
 function sleep(ms) {
-	return new Promise((resolve) => {
-		setTimeout(resolve, ms);
-	});
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
 }
 
 function GetImageUrl(post) {
@@ -51,69 +59,71 @@ function GetImageUrl(post) {
 	}
 }
 
-async function ProcessPost(post) {
-	let imageUrl = GetImageUrl(post);
+async function ProcessPost(post, subreddit) {
+    let imageUrl = GetImageUrl(post);
 
-	let embed = {
-		title: `${subreddit} - ${post.title}`.substring(0, 256),
-		description: post.selftext.substring(0, 512),
-		url: `https://www.reddit.com${post.permalink}`,
-		color: 0xFF5700,
-		author: {
-			name: `u/${post.author}`,
-			url: `https://www.reddit.com/user/${post.author}`
-		},
-		image: imageUrl ? {
-			url: imageUrl
-		} : null
-	}
+    let embed = {
+        title: `${subreddit.name} - ${post.title}`.substring(0, 256),
+        description: post.selftext.substring(0, 512),
+        url: `https://www.reddit.com${post.permalink}`,
+        color: 0xFF5700,
+        author: {
+            name: `u/${post.author}`,
+            url: `https://www.reddit.com/user/${post.author}`,
+        },
+        image: imageUrl ? {
+            url: imageUrl,
+        } : null,
+    };
 
-	try {
-		await axios.post(`https://discordapp.com/api/webhooks/${process.env.WEBHOOK_ID}/${process.env.WEBHOOK_TOKEN}`, {
-			embeds: [embed]
-		});
-		await sleep(2000);
-	} catch(ex) {
-		console.error("Failed to send post", embed, "because", ex.response.data)
-	}
+    try {
+        // Append '?thread_id=' to the webhook URL
+        await axios.post(`https://discordapp.com/api/webhooks/${process.env.WEBHOOK_ID}/${process.env.WEBHOOK_TOKEN}?thread_id=${subreddit.threadId}`, {
+            embeds: [embed],
+        });
+        await sleep(2000);
+    } catch (ex) {
+        console.error("Failed to send post", embed, "because", ex.response.data);
+    }
 }
 
 async function main() {
-	let response;
-	try {
-		response = await axios.get(`https://www.reddit.com/${subreddit}/new.json`);
-	} catch(ex) {
-		console.error("Got error", ex?.message || ex)
-		return setTimeout(main, 60 * 1000);
-	}
-	let posts = response.data.data.children;
+    let response;
+    try {
+        // Iterate through each subreddit
+        for (const subreddit of subreddits) {
+            response = await axios.get(`https://www.reddit.com/${subreddit.name}/new.json`);
+            let posts = response.data.data.children;
 
-	if(!lastPost) {
-		lastPost = {date: posts[0].data.created, id: posts[0].data.name};
-		return setTimeout(main, 60 * 1000);
-	}
+            if (!lastPost[subreddit.name]) {
+                lastPost[subreddit.name] = { date: posts[0].data.created, id: posts[0].data.name };
+                continue;
+            }
 
-	let toSend = [];
+            let toSend = [];
 
-	for(let post of posts) {
-		if(post.data.name === lastPost.id || post.data.created <= lastPost.date){ //have to use this because if someone deletes their post then the "before" get param breaks, also <= is needed instead of < so that it doesn't spam if the last post id was deleted and there was another post created at the same second
-			break;
-		}
-		toSend.push(post.data);
-	}
+            for (let post of posts) {
+                if (post.data.name === lastPost[subreddit.name].id || post.data.created <= lastPost[subreddit.name].date) {
+                    break;
+                }
+                toSend.push(post.data);
+            }
 
-	if(toSend.length === 0)
-		return setTimeout(main, 60 * 1000);
+            if (toSend.length > 0) {
+                for (let post of toSend.reverse()) {
+                    console.log(`Processing post for ${subreddit.name}`, post.name, "-", post.title);
+                    await ProcessPost(post, subreddit);
+                }
 
-	for(let post of toSend.reverse()) {
-		console.log("Processing post", post.name, "-", post.title)
-		await ProcessPost(post);
-	}
+                lastPost[subreddit.name] = { date: toSend.at(-1).created, id: toSend.at(-1).name };
+            }
+        }
+    } catch (ex) {
+        console.error("Got error", ex?.message || ex);
+    }
 
-	lastPost = {date: toSend.at(-1).created, id: toSend.at(-1).name};
-	fs.writeFileSync("last.dat", JSON.stringify(lastPost));
-
-	setTimeout(main, 60 * 1000);
+    fs.writeFileSync("last.dat", JSON.stringify(lastPost));
+    setTimeout(main, 60 * 1000);
 }
 
 main();
